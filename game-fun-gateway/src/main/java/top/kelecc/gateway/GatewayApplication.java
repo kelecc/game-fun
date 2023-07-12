6f92f8b474ba87b1f39abc18fff46f512bce2208
kele <22256514@qq.com>
2023-07-12 21:53:26 +0800

    初始化Gateway模块

---
+++ game-fun-gateway/src/main/java/top/kelecc/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package top.kelecc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 可乐
+ * @version 1.0
+ * @description:
+ * @date 2023/7/12 20:44
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
