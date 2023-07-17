767b3f544b1c1d161e00b9bd713b9167ed52a23c
kele <22256514@qq.com>
2023-07-17 16:23:19 +0800

    完善Api文档注解并添加validation校验

--- game-fun-model/src/main/java/top/kelecc/model/user/dto/WmUserLoginDto.java
+++ game-fun-model/src/main/java/top/kelecc/model/user/dto/WmUserLoginDto.java
@@ -1,7 +1,10 @@
 package top.kelecc.model.user.dto;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author 可乐
@@ -12,9 +15,13 @@ public class WmUserLoginDto {
     /**
      * 用户名
      */
+    @ApiModelProperty(value = "用户名", dataType = "String", example = "admin", required = true)
+    @NotBlank(message = "用户名不能为空")
     private String name;
     /**
      * 密码
      */
+    @ApiModelProperty(value = "密码", dataType = "String", example = "123456", required = true)
+    @NotBlank(message = "密码不能为空")
     private String password;
 }
