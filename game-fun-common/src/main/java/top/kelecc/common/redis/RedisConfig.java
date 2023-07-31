b9d3fa1714436ff5b5fb1be95ae220e2384036ea
kele <22256514@qq.com>
2023-07-31 14:51:29 +0800

    在redis-stater之前注入redisTemplate

--- game-fun-common/src/main/java/top/kelecc/common/redis/RedisConfig.java
+++ game-fun-common/src/main/java/top/kelecc/common/redis/RedisConfig.java
@@ -1,5 +1,6 @@
 package top.kelecc.common.redis;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,6 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@AutoConfigureBefore(org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class)
 public class RedisConfig {
 
     @Bean
